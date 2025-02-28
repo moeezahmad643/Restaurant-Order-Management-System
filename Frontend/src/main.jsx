e33165eaad06acb6efebfa797a2065caae9e19b7
Moeez <moiz27673855@gmail.com>
2025-02-27 20:21:13 -0800

    Some changes

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,12 +1,10 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Navbar from "./components/NavBar/Navbar.jsx";
+import Navbar from "./components/Navbar/Navbar.jsx";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Admin from "./admin.jsx";
 import Orderes from "./components/Orderrd/Orderes.jsx";
-
-
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
@@ -20,12 +18,7 @@ createRoot(document.getElementById("root")).render(
           </>
         }
       />
-      <Route
-        path="/ordered/*"
-        element={
-          <Orderes/>
-        }
-      />
+      <Route path="/ordered/*" element={<Orderes />} />
       <Route
         path="/admin"
         element={
