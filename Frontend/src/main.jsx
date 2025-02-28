36c48cddd11b3b9c98d0acb2ea79a42a7e24783c
Moeez <moiz27673855@gmail.com>
2025-02-27 20:54:38 -0800

    some new changes

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,8 +1,8 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Navbar from "./components/NavBar/Navbar.jsx";
 import { BrowserRouter, Routes, Route } from "react-router";
+import Navbar from "./components/NavBar/Navbar.jsx";
 import Admin from "./admin.jsx";
 import Orderes from "./components/Orderrd/Orderes.jsx";
 
